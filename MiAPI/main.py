6f1b5ae1ace2379e84b019f63f7c0a0618699294
isabellaaaaaaaaaaaaa <isabella.castal12@gmail.com>
2026-02-10 22:59:01 -0600

    practica 2 endpoints

--- MiAPI/main.py
+++ MiAPI/main.py
@@ -1,19 +1,50 @@
-#Importancionescd..
+# Importaciones
 from fastapi import FastAPI
+import asyncio
+from typing import Optional
 
-#nuevaaa 
-import asyncio 
+# Instancia del servidor
+app = FastAPI(
+    title="Mi primer API",
+    description="Isabella Castro Alavez",
+    version="1.0.0"
+)
 
-#Intstancia del servidor
-app = FastAPI()
+# BD ficticia
+usuarios = [
+    {"id": 1, "nombre": "Juan", "edad": 21},
+    {"id": 2, "nombre": "Israel", "edad": 21},
+    {"id": 3, "nombre": "Sofi", "edad": 21}
+]
 
-#Endpoints
-@app.get("/")
+# Endpoints
+@app.get("/", tags=["Inicio"])
 async def bienvenido():
-    return {"Mensaje": "Bienvenido a mi APi"}
+    return {"Mensaje": "Bienvenido a mi API"}
 
-@app.get("/Hola Mundo")
+@app.get("/Hola Mundo", tags=["Bienvenida Asincrona"])
 async def Hola():
-    await asyncio.sleep(7) #simular como si estuviera cargandi 
+    await asyncio.sleep(7)  # simula carga
+    return {"Mensaje": "Bienvenido a mi API"}
 
-    return {"Mensaje": "Bienvenido a mi APi"}
+@app.get("/v1/usuario/{id}", tags=["Parametro Obligatorio"])
+async def consultaUno(id: int):
+    return {"Se encontro usuario": id}
+
+@app.get("/v1/usuarios/", tags=["Parametro Opcional"])
+async def consultaTodos(id: Optional[int] = None):
+    if id is not None:
+        for usuario in usuarios:
+            if usuario["id"] == id:
+                return {
+                    "mensaje": "usuario encontrado",
+                    "usuario": usuario
+                }
+        return {
+            "mensaje": "usuario no encontrado",
+            "usuario": id
+        }
+    else:
+        return {
+            "mensaje": "No se proporcionó id"
+        }
